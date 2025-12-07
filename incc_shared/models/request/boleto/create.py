668a01632868b5aa4515a37d17d0fcb1a3acb8cf
jrt <lucasjrt99@gmail.com>
2025-12-06 22:27:31 -0300

    fix: emissao should be optional when issuing boleto

--- incc_shared/models/request/boleto/create.py
+++ incc_shared/models/request/boleto/create.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import List, Optional
 
 from pydantic import Field
@@ -9,5 +10,6 @@ from incc_shared.models.db.boleto.base import BoletoBase, StatusBoleto
 class CreateBoletoModel(BoletoBase):
     nossoNumero: int = Field(0, exclude=True)
     status: List[StatusBoleto] = Field([], exclude=True)
+    emissao: date = Field(date.today())
     juros: Optional[Juros] = None
     multa: Optional[Juros] = None
