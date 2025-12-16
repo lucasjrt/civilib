d2f5d42ea19fa9553a365fa41d4f1736c1d8da5a
jrt <lucasjrt99@gmail.com>
2025-12-15 21:14:13 -0300

    chore: add type hints to mapping.py

--- incc_shared/utils/mapping.py
+++ incc_shared/utils/mapping.py
@@ -1,4 +1,9 @@
-def index_customers(customers):
+from typing import Dict, List
+
+from incc_shared.models.db.customer.customer import CustomerModel
+
+
+def index_customers(customers: List[CustomerModel]) -> Dict[str, CustomerModel]:
     customer_index = {}
     for customer in customers:
         customer_index[customer.customerId] = customer
