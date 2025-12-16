4f5181c8dee729a830496247ab02184e4d867d97
jrt <lucasjrt99@gmail.com>
2025-12-15 21:15:32 -0300

    chore: fix customer key to ulid

--- incc_shared/utils/mapping.py
+++ incc_shared/utils/mapping.py
@@ -1,9 +1,11 @@
 from typing import Dict, List
 
+from ulid import ULID
+
 from incc_shared.models.db.customer.customer import CustomerModel
 
 
-def index_customers(customers: List[CustomerModel]) -> Dict[str, CustomerModel]:
+def index_customers(customers: List[CustomerModel]) -> Dict[ULID, CustomerModel]:
     customer_index = {}
     for customer in customers:
         customer_index[customer.customerId] = customer
